8f703c6e3bf4160b9f4f81db757c9e427bcec155
BarryFu <barrelcact309@gmail.com>
2016-08-30 10:46:47 +0800

    py-note

---
+++ hello.py
@@ -0,0 +1,87 @@
+# print("Hello Python!")
+#
+# PI = 3.14
+# DAY_OF_YEAR = 365
+# first_name = 'Bary'
+# last_name = "Fu"
+#
+# print(PI)
+# print(DAY_OF_YEAR / 3)
+# print(first_name, last_name)
+#
+# print(first_name == "Bary")
+#
+# print("day" in "Friday")
+#
+# print(len(first_name))
+#
+# print(first_name[::2])
+#
+
+data = """B10231011:x:1067:1000:Su Lin-Ya,,0910-622-175:/home/B10231011:/bin/bash
+B10206009:x:1068:1000:JI-BING-CHEN,,0910246189:/home/B10206009:/bin/bash
+B10206035:x:1070:1000:ZHANG-JIE-KAI,,0970663675:/home/B10206035:/bin/bash
+B10206031:x:1071:1000:LI ZHE-CHENG,,0903008989:/home/B10206031:/bin/bash
+D10406811:x:1072:1000:jemal yimer damte,,0965668144:/home/D10406811:/bin/bash
+M10406013:x:1073:1000:wang kai-ting,,0988148729:/home/M10406013:/bin/bash
+B10206022:x:1074:1000:SU-BO-CHENG,,0970623878:/home/B10206022:/bin/bash
+M10406009:x:1075:1000:Yu Ching,,0987930004:/home/M10406009:/bin/bash
+tino:x:1076:1000:Kaiting wang,,0988148729:/home/tino:/bin/bash
+ychungn:x:1077:1000:Hung Ying Chieh,,0975207247:/home/ychungn:/bin/bash
+D10306103:x:1078:1000:Yu-Cheng Liu,,0960537760:/home/D10306103:/bin/bash
+D10406817:x:1079:1000:Dhana Lakshmi Busipalli,0966747020:/home/D10406817:/bin/bash
+chya:x:1080:1000:chya,0956015982:/home/chya:/bin/bash"""
+
+
+
+
+
+
+
+
+
+
+# weeks = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# for day in weeks:
+#     if day == "Saturday" or day == "Sunday":
+#         print("Holiday is : " + day)
+#     else:
+#         print("Workday is : " + day )
+#
+# input_day = "FRI"
+#
+# for day in weeks:
+#     if input_day.lower() in day.lower():
+#         print(day)
+#     else:
+#         print(None)
+#
+#
+#
+# week = {'mon': "Monday",
+#         'tues': "Tuesday",
+#         'wed': "Wednesday",
+#         'thur': "Thursday",
+#         'fri': "Friday",
+#         'sat': "Saturday",
+#         'sun': "Sunday"}
+#
+# week.setdefault(input_day.lower(), 'None')
+# print(week[input_day.lower()])
+
+
+
+
+
+
+# pork = {'A': 1, 'K': 13, 'Q':12}
+# print(pork['J'] )
+
+# pork_number = 'N'
+# default_val=0
+#
+# pork.setdefault(pork_number, default_val)
+# if not(pork_number in pork.keys()):
+#     pork[pork_number] = default_val
+
+# print(pork[pork_number])
---
+++ hw.py
@@ -0,0 +1,21 @@
+user = {
+"B10231011":"x:1067:1000:Su Lin-Ya,,0910-622-175:/home/B10231011:/bin/bash",
+"B10206009":"x:1068:1000:JI-BING-CHEN,,0910246189:/home/B10206009:/bin/bash",
+"B10206035":"x:1070:1000:ZHANG-JIE-KAI,,0970663675:/home/B10206035:/bin/bash",
+"B10206031":"x:1071:1000:LI ZHE-CHENG,,0903008989:/home/B10206031:/bin/bash",
+"D10406811":"x:1072:1000:jemal yimer damte,,0965668144:/home/D10406811:/bin/bash",
+"M10406013":"x:1073:1000:wang kai-ting,,0988148729:/home/M10406013:/bin/bash",
+"B10206022":"x:1074:1000:SU-BO-CHENG,,0970623878:/home/B10206022:/bin/bash",
+"M10406009":"x:1075:1000:Yu Ching,,0987930004:/home/M10406009:/bin/bash",
+"tino":"x:1076:1000:Kaiting wang,,0988148729:/home/tino:/bin/bash",
+"ychungn":"x:1077:1000:Hung Ying Chieh,,0975207247:/home/ychungn:/bin/bash",
+"D10306103":"x:1078:1000:Yu-Cheng Liu,,0960537760:/home/D10306103:/bin/bash",
+"D10406817":"x:1079:1000:Dhana Lakshmi Busipalli,0966747020:/home/D10406817:/bin/bash",
+"chya":"x:1080:1000:chya,0956015982:/home/chya:/bin/bash"}
+
+
+user_name = 'B10206009'
+default_val= "none"
+user.setdefault(uer, default_val)
+
+
